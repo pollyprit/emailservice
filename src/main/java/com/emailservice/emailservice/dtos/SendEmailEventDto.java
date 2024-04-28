7926a46dd235148100266774d7ae382a27c9cf3c
polly <66064010+pollyprit@users.noreply.github.com>
2024-04-28 08:11:58 +0530

    e-commerce app Email micro-service
    e-commerce app Email micro-service

---
+++ src/main/java/com/emailservice/emailservice/dtos/SendEmailEventDto.java
@@ -0,0 +1,14 @@
+package com.emailservice.emailservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SendEmailEventDto {
+    private String to;
+    private String from;
+    private String subject;
+    private String body;
+}
+
